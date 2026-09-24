7162db76fb548cdf686055500770af56273560b0
ashkan motaei <ashkan.mataee@yahoo.com>
2026-09-24 13:02:30 +0330

    redesign
    redesign

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='ashkan-portfolio-v2';const A=['./','./index.html','./css/style.css','./js/app.js','./js/pages.js','./img/person.jpg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
